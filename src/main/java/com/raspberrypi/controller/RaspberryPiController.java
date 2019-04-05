f236ddb24f06a75e3df3d797948eb7ab63570a7c
Anand Gajjar <anand.gajjar@hpe.com>
2019-04-05 20:12:38 +0530

    no message

--- src/main/java/com/raspberrypi/controller/RaspberryPiController.java
+++ src/main/java/com/raspberrypi/controller/RaspberryPiController.java
@@ -1,7 +1,6 @@
 package com.raspberrypi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pi4j.io.gpio.GpioController;
@@ -13,16 +12,58 @@ import com.pi4j.io.gpio.RaspiPin;
 @RestController
 public class RaspberryPiController {
 	
-	public static GpioPinDigitalOutput pin;
-	
+
 	@GetMapping("/raspberry/light")
 	public String light(){
-		if(pin==null){
-			GpioController gpio = GpioFactory.getInstance();
-			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25,"My LED",PinState.LOW);
-		}
-		pin.toggle();
-		return "Response from light!!";
+		try {
+	      System.out.println("<--Pi4J--> GPIO Control Example ... started.");
+
+	        // create gpio controller
+	        final GpioController gpio = GpioFactory.getInstance();
+
+	        // provision gpio pin #01 as an output pin and turn on
+	        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+
+	        // set shutdown state for this pin
+	        pin.setShutdownOptions(true, PinState.LOW);
+
+	        System.out.println("--> GPIO state should be: ON");
+
+	        Thread.sleep(5000);
+
+	        // turn off gpio pin #01
+	        pin.low();
+	        System.out.println("--> GPIO state should be: OFF");
+
+	        Thread.sleep(5000);
+
+	        // toggle the current state of gpio pin #01 (should turn on)
+	        pin.toggle();
+	        System.out.println("--> GPIO state should be: ON");
+
+	        Thread.sleep(5000);
+
+	        // toggle the current state of gpio pin #01  (should turn off)
+	        pin.toggle();
+	        System.out.println("--> GPIO state should be: OFF");
+	        
+			Thread.sleep(5000);
+			
+
+	        // turn on gpio pin #01 for 1 second and then off
+	        System.out.println("--> GPIO state should be: ON for only 1 second");
+	        pin.pulse(1000, true); // set second argument to 'true' use a blocking call
+
+	        // stop all GPIO activity/threads by shutting down the GPIO controller
+	        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+	        gpio.shutdown();
+	        
+	        } catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			return "Exiting ControlGpioExample";
 	}
 
 }
