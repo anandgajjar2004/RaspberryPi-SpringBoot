e7326bd620233119b777d2a493a8d0468a2e7563
Anand Gajjar <anand.gajjar@hpe.com>
2019-04-05 20:15:09 +0530

    no message

--- src/main/java/com/raspberrypi/controller/RaspberryPiController.java
+++ src/main/java/com/raspberrypi/controller/RaspberryPiController.java
@@ -22,7 +22,7 @@ public class RaspberryPiController {
 	        final GpioController gpio = GpioFactory.getInstance();
 
 	        // provision gpio pin #01 as an output pin and turn on
-	        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+	        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, "MyLED", PinState.HIGH);
 
 	        // set shutdown state for this pin
 	        pin.setShutdownOptions(true, PinState.LOW);
